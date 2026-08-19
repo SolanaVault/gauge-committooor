0acb1112b5c020e85524fdca1ebd7aabe1d08b4a
x <x>
2026-08-19 15:14:38 +0200

    Remove temporary GitHub token verifier

--- verify-github-token.ts
+++
@@ -1,6 +0,0 @@
-import { readFile } from "node:fs/promises";
-
-import { saveDataToGitHub } from "./github.ts";
-
-const content = await readFile("last_parsed_epoch", "utf8");
-await saveDataToGitHub([{ path: "last_parsed_epoch", content }]);
